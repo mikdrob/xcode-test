183e449a40a805f5feb57341d43408796b0a578d
midrob <midrob@ttu.ee>
2021-09-15 20:31:18 +0300

    Initial Commit

---
+++ AppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTest
+//
+//  Created by Mikhail Drobyshev on 15.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
